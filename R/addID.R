f4c96d728627cebca4fb6dc7b5aad1a983e50bda
Davide Morselli <davide.morselli@unil.ch>
2014-02-28 00:00:00 +0000

    version 0.5-7

--- R/addID.R
+++
@@ -1,9 +0,0 @@
-addID <-
-function(poly,correspondance,area,name){
-  l <- nrow(poly@data)
-  poly@data[[name]] <- rep(0,nrow(poly@data))
-  for (i in 1:l){
-    poly@data[[name]][poly@data[[area]]==correspondance[i]] <-i
-  }
-  return(poly)
-}
